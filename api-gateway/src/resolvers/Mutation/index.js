eb7ba1c04bba007fbfbe1668209b323278161fff
Brian Yingling <brian@jet.com>
2020-01-21 00:30:53 -0500

    eslint fixes

--- api-gateway/src/resolvers/Mutation/index.js
+++ api-gateway/src/resolvers/Mutation/index.js
@@ -1,16 +1,14 @@
 import { UsersService } from '#root/adapters';
 
-const createUser = async (parent, { email, password }, context, info) => {
-    return await UsersService.createUser(email, password);
-}
+const createUser = async (parent, { email, password }) => UsersService.createUser(email, password);
 
-const createSession = async (parent, { email, password }, context, info) => {
-    const session = await UsersService.createSession(email, password);
-    context.res.cookie('sessionId', session.id, { httpOnly: true });
-    return session;    
-}
+const createSession = async (parent, { email, password }, context) => {
+  const session = await UsersService.createSession(email, password);
+  context.res.cookie('sessionId', session.id, { httpOnly: true });
+  return session;
+};
 
 export {
-    createSession,
-    createUser
-}
+  createSession,
+  createUser,
+};
