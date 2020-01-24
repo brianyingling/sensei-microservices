ed16baf850337b518794d9cfd0b1e6271c5c2767
Brian Yingling <brian@jet.com>
2020-01-24 13:12:19 -0500

    added eslint to readings-service and users-service

--- readings-service/src/routes.js
+++ readings-service/src/routes.js
@@ -3,15 +3,13 @@ import getReadings from './handlers/getReadings';
 import getReadingsByLocation from './handlers/getReadingsByLocation';
 import getLatestReadingsByLocation from './handlers/getLatestReadingsByLocation';
 
-const routes = app => {
-    app.get('/helloworld', (req, res, next) => {
-        return res.send('hello world');
-    });
+const routes = (app) => {
+  app.get('/helloworld', (req, res, next) => res.send('hello world'));
 
-    app.get('/readings', getReadings);
-    app.post('/readings', createReading);
-    app.get('/locations/:id/readings', getReadingsByLocation);
-    app.get('/locations/readings/latest', getLatestReadingsByLocation);
-}
+  app.get('/readings', getReadings);
+  app.post('/readings', createReading);
+  app.get('/locations/:id/readings', getReadingsByLocation);
+  app.get('/locations/readings/latest', getLatestReadingsByLocation);
+};
 
 export default routes;
